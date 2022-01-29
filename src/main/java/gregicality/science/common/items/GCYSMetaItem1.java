c96362b88ea517c5258e5201ceff76ab0888c581
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2022-01-29 19:10:52 +0100

    Added circuits

--- src/main/java/gregicality/science/common/items/GCYSMetaItem1.java
+++ src/main/java/gregicality/science/common/items/GCYSMetaItem1.java
@@ -1,8 +1,11 @@
 package gregicality.science.common.items;
 
 import gregtech.api.items.metaitem.StandardMetaItem;
+import gregtech.api.unification.material.MarkerMaterial;
+import gregtech.api.unification.material.MarkerMaterials;
+import gregtech.api.unification.ore.OrePrefix;
 
-import static gregicality.science.common.items.GCYSMetaItems.MAGNETRON;
+import static gregicality.science.common.items.GCYSMetaItems.*;
 
 public class GCYSMetaItem1 extends StandardMetaItem {
 
@@ -13,5 +16,30 @@ public class GCYSMetaItem1 extends StandardMetaItem {
     @Override
     public void registerSubItems() {
         MAGNETRON = addItem(0, "magnetron");
+
+        BIOWARE_PROCESSOR = addItem(1, "circuit.bioware_processor").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Ultimate);
+        BIOWARE_ASSEMBLY = addItem(2, "circuit.bioware_assembly").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Super);
+        BIOWARE_COMPUTER = addItem(3, "circuit.bioware_computer").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Infinite);
+        BIOWARE_MAINFRAME = addItem(4, "circuit.bioware_mainframe").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Ultra);
+
+        OPTICAL_PROCESSOR = addItem(5,"circuit.optical_processor").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Super);
+        OPTICAL_ASSEMBLY = addItem(6,"circuit.optical_assembly").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Infinite);
+        OPTICAL_COMPUTER = addItem(7,"circuit.optical_computer").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Ultra);
+        OPTICAL_MAINFRAME = addItem(8,"circuit.optical_mainframe").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Insane);
+
+        EXOTIC_PROCESSOR = addItem(9, "circuit.exotic_processor").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Infinite);
+        EXOTIC_ASSEMBLY = addItem(10, "circuit.exotic_assembly").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Ultra);
+        EXOTIC_COMPUTER = addItem(11, "circuit.exotic_computer").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Insane);
+        EXOTIC_MAINFRAME = addItem(12, "circuit.exotic_mainframe").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Epic);
+
+        COSMIC_PROCESSOR = addItem(13, "circuit.cosmic_processor").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Ultra);
+        COSMIC_ASSEMBLY = addItem(14, "circuit.cosmic_assembly").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Insane);
+        COSMIC_COMPUTER = addItem(15, "circuit.cosmic_computer").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Epic);
+        COSMIC_MAINFRAME = addItem(16, "circuit.cosmic_mainframe").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Legendary);
+
+        SUPRACAUSAL_PROCESSOR = addItem(17, "circuit.supracausal_processor").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Insane);
+        SUPRACAUSAL_ASSEMBLY = addItem(18, "circuit.supracausal_assembly").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Epic);
+        SUPRACAUSAL_COMPUTER = addItem(19, "circuit.supracausal_computer").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Legendary);
+        SUPRACAUSAL_MAINFRAME = addItem(20, "circuit.supracausal_mainframe").setUnificationData(OrePrefix.circuit, MarkerMaterials.Tier.Maximum);
     }
 }
--- src/main/java/gregicality/science/common/items/GCYSMetaItems.java
+++ src/main/java/gregicality/science/common/items/GCYSMetaItems.java
@@ -7,6 +7,31 @@ public class GCYSMetaItems {
 
     public static MetaItem<?>.MetaValueItem MAGNETRON;
 
+    public static MetaItem<?>.MetaValueItem BIOWARE_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem BIOWARE_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem BIOWARE_COMPUTER;
+    public static MetaItem<?>.MetaValueItem BIOWARE_MAINFRAME;
+
+    public static MetaItem<?>.MetaValueItem OPTICAL_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem OPTICAL_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem OPTICAL_COMPUTER;
+    public static MetaItem<?>.MetaValueItem OPTICAL_MAINFRAME;
+
+    public static MetaItem<?>.MetaValueItem EXOTIC_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem EXOTIC_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem EXOTIC_COMPUTER;
+    public static MetaItem<?>.MetaValueItem EXOTIC_MAINFRAME;
+
+    public static MetaItem<?>.MetaValueItem COSMIC_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem COSMIC_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem COSMIC_COMPUTER;
+    public static MetaItem<?>.MetaValueItem COSMIC_MAINFRAME;
+
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_COMPUTER;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_MAINFRAME;
+
 
     public static void init() {
         GCYSMetaItem1 metaItem1 = new GCYSMetaItem1();
