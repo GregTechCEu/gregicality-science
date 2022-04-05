deb694b8f9e310c88c0a8bb028174d151ceeb4f1
brachy84 <brachy84@gmail.com>
2022-04-05 17:44:50 +0200

    yeet old stuff

--- src/main/java/gregicality/science/api/capability/GCYSTileCapabilities.java
+++ src/main/java/gregicality/science/api/capability/GCYSTileCapabilities.java
@@ -1,17 +1,9 @@
 package gregicality.science.api.capability;
 
-import gregicality.science.api.metatileentity.IVacuumConsumer;
-import gregicality.science.api.metatileentity.IVacuumProducer;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 
 public class GCYSTileCapabilities {
-
-    @CapabilityInject(IVacuumProducer.class)
-    public static Capability<IVacuumProducer> CAPABILITY_VACUUM_PRODUCER = null;
-
-    @CapabilityInject(IVacuumConsumer.class)
-    public static Capability<IVacuumConsumer> CAPABILITY_VACUUM_CONSUMER = null;
 
     @CapabilityInject(IPressureContainer.class)
     public static Capability<IPressureContainer> CAPABILITY_PRESSURE_CONTAINER = null;
--- src/main/java/gregicality/science/api/capability/IPressureContainer.java
+++ src/main/java/gregicality/science/api/capability/IPressureContainer.java
@@ -1,40 +1,92 @@
 package gregicality.science.api.capability;
 
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * A container which can handle pressures. All values units are in Pa
+ */
 public interface IPressureContainer {
 
-    static final double ATMOSPHERIC_PRESSURE = 101325;
+    /**
+     * Default pressure. Anything below this counts a vacuum
+     */
+    double ATMOSPHERIC_PRESSURE = 101325;
 
+    /**
+     * @return the current pressure hold by this container
+     */
     double getPressure();
 
-    void applyPressure(double amount);
+    /**
+     * Changes the pressure by the given amount
+     *
+     * @param amount amount to change. + for add - for subtract
+     * @return actually changed amount
+     */
+    double changePressure(double amount);
 
+    /**
+     * @return the maximum pressure this container can handle
+     */
     double getMaxPressure();
 
+    /**
+     * @return the minimum pressure this container can handle
+     */
     default double getMinPressure() {
-        return 1 / getMaxPressure();
+        return ATMOSPHERIC_PRESSURE / getMaxPressure();
     }
 
+    /**
+     * @return if the container currently holds a vacuum
+     */
     default boolean isVacuum() {
-        return getPressure() < ATMOSPHERIC_PRESSURE - 50;
+        return getPressure() < ATMOSPHERIC_PRESSURE - 1000;
     }
 
+    /**
+     * @return if the pressure is around atmospheric levels
+     */
     default boolean isNormalPressure() {
-        return getPressure() < ATMOSPHERIC_PRESSURE + 50 && getPressure() > ATMOSPHERIC_PRESSURE - 50;
+        return getPressure() < ATMOSPHERIC_PRESSURE + 1000 && getPressure() > ATMOSPHERIC_PRESSURE - 1000;
     }
 
-    static final IPressureContainer EMPTY = new IPressureContainer() {
+    /**
+     * Determines whether this container can handle the given pressure
+     *
+     * @param pressure pressure to check
+     * @return if pressure can be handled
+     */
+    default boolean canHandlePressure(double pressure) {
+        return pressure >= getMinPressure() && pressure <= getMaxPressure();
+    }
+
+    static void causePressureExplosion(boolean vacuum, World world, BlockPos pos) {
+        if (world != null && !world.isRemote) {
+            if (vacuum) {
+                //TODO implosion
+                world.createExplosion(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 5, true);
+            } else {
+                world.createExplosion(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 5, true);
+            }
+        }
+    }
+
+    IPressureContainer EMPTY = new IPressureContainer() {
         @Override
         public double getPressure() {
+            return ATMOSPHERIC_PRESSURE;
+        }
+
+        @Override
+        public double changePressure(double amount) {
             return 0;
         }
 
         @Override
-        public void applyPressure(double amount) {
-        }
-
-        @Override
         public double getMaxPressure() {
-            return ATMOSPHERIC_PRESSURE;
+            return ATMOSPHERIC_PRESSURE * 2;
         }
     };
 }
