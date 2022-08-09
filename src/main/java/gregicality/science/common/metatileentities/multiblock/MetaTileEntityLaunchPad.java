11468642402dbc0b0c22ce33aa244e692c55c21f
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2022-08-09 13:56:21 +0200

    Big rebase Added launch pad structure

--- src/main/java/gregicality/science/common/metatileentities/multiblock/MetaTileEntityLaunchPad.java
+++ src/main/java/gregicality/science/common/metatileentities/multiblock/MetaTileEntityLaunchPad.java
@@ -1,8 +1,9 @@
 package gregicality.science.common.metatileentities.multiblock;
 
-import gregicality.multiblocks.api.render.GCYMTextures;
+import gregicality.multiblocks.api.unification.GCYMMaterials;
 import gregicality.science.api.recipes.GCYSRecipeMaps;
-import gregicality.science.client.render.GCYSTextures;
+import gregicality.science.common.block.GCYSMetaBlocks;
+import gregicality.science.common.block.blocks.BlockMultiblockCasing;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
@@ -10,6 +11,10 @@ import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.BlockStoneSmooth;
+import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.util.ResourceLocation;
 
 public class MetaTileEntityLaunchPad extends RecipeMapMultiblockController {
@@ -26,12 +31,33 @@ public class MetaTileEntityLaunchPad extends RecipeMapMultiblockController {
 
     @Override
     public BlockPattern createStructurePattern(){
-        return FactoryBlockPattern.start().build();
+        return FactoryBlockPattern.start()
+                .aisle("   CCC   ", "  CAAAC  ", "  CAAAC  ", "  C   C  ", "   CCC   ", "   III   ", "   PPP   ", "   PPP   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "         ", "         ", "         ")
+                .aisle("   CCC   ", " CCAAACC ", " CCAAACC ", " CCCCCCC ", "  BCCCB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  IIIII  ")
+                .aisle("   CCC   ", " CCAAACC ", " CCAAACC ", " CCCCCCC ", "  IIIII  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "    I    ")
+                .aisle("   CCC   ", "CCCAAACCC", "CCCAAACCC", " CCCCCCC ", "  CCCCC  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ")
+                .aisle("CCCCCCCCC", "AAAAAAAAA", "AAAAAAAAA", "CCCAAACCC", " CCFFFCC ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ")
+                .aisle("CCCCCCCCC", "AAAAAAAAA", "AAAAAAAAA", "CCCAAACCC", " CCFFFCC ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ")
+                .aisle("CCCCCCCCC", "AAAAAAAAA", "AAAAAAAAA", "CCCAAACCC", " CCFFFCC ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ")
+                .aisle("   CCC   ", "CCCAAACCC", "CCCAAACCC", " CCCCCCC ", "  CCCCC  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ", "  AAAAA  ")
+                .aisle("   CCC   ", " CCAAACC ", " CCAAACC ", " CCCCCCC ", "  IIIII  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FATAF  ", "  FATAF  ", "  FATAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "  FABAF  ", "    I    ")
+                .aisle("   CCC   ", " CCAAACC ", " CCAAACC ", " CCCCCCC ", "  BCCCB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPTPB  ", "  BPTPB  ", "  BPTPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  BPBPB  ", "  IIIII  ")
+                .aisle("   CCC   ", "  CAAAC  ", "  CAAAC  ", "  C   C  ", "   CCC   ", "   ISI   ", "   PPP   ", "   PPP   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   B B   ", "   III   ", "         ", "         ", "         ")
+                .where('S', selfPredicate())
+                .where('C', states(MetaBlocks.STONE_SMOOTH.getState(BlockStoneSmooth.BlockType.CONCRETE_LIGHT)).or(states(MetaBlocks.STONE_SMOOTH.getState(BlockStoneSmooth.BlockType.CONCRETE_DARK))))
+                .where('A', air())
+                .where('F', states(MetaBlocks.FRAMES.get(GCYMMaterials.HSLASteel).getBlock(GCYMMaterials.HSLASteel)))
+                .where('I', states(MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID)))
+                .where('B', states(GCYSMetaBlocks.MULTIBLOCK_CASING.getState(BlockMultiblockCasing.CasingType.STRUCTURAL_BEAM)))
+                .where('T', states(GCYSMetaBlocks.MULTIBLOCK_CASING.getState(BlockMultiblockCasing.CasingType.TOWER_INSTRUMENTS)))
+                .where('P', states(GCYSMetaBlocks.MULTIBLOCK_CASING.getState(BlockMultiblockCasing.CasingType.TOWER_PIPING)))
+                .where(' ', any())
+                .build();
         //TODO: Create pattern
     }
 
     @Override
     public ICubeRenderer getBaseTexture(IMultiblockPart iMultiblockPart) {
-        return GCYSTextures.LAUNCHPAD_OVERLAY;
+        return Textures.SOLID_STEEL_CASING;
     }
 }
